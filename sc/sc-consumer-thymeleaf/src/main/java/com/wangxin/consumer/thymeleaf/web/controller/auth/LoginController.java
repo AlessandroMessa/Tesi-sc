ccaadc306b137e9ec22dfdf3a133bf2623c89628
Diagon <a.messa7@campus.unimib.it>
2025-08-05 13:55:32 +0200

    ref2-test- mi aspetto 5 smells

--- sc/sc-consumer-thymeleaf/src/main/java/com/wangxin/consumer/thymeleaf/web/controller/auth/LoginController.java
+++ sc/sc-consumer-thymeleaf/src/main/java/com/wangxin/consumer/thymeleaf/web/controller/auth/LoginController.java
@@ -1,6 +1,7 @@
 package com.wangxin.consumer.thymeleaf.web.controller.auth;
 
-import com.wangxin.consumer.contract.auth.dto.UserDto;
+
+import com.wangxin.common.api.model.auth.User;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
@@ -26,21 +27,21 @@ public class LoginController {
 
     @RequestMapping(value = "login", method = RequestMethod.GET)
     String login(Model model) {
-        model.addAttribute("user", new UserDto());
+        model.addAttribute("user", new User());
         log.info("#去登录");
         return "view/login/login";
     }
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public String login(@ModelAttribute("userForm") UserDto user, RedirectAttributes redirectAttributes) {
+    public String login(@ModelAttribute("userForm") User user, RedirectAttributes redirectAttributes) {
         log.info("# 登录中 ");
-        if (null == user || StringUtils.isBlank(user.getUsername()) || StringUtils.isBlank(user.getPasswordHash())) {
+        if (null == user || StringUtils.isBlank(user.getUsername()) || StringUtils.isBlank(user.getPassword())) {
             log.error("# 账号或密码错误");
             return "login";
         }
 
         String username = user.getUsername();
-        UsernamePasswordToken token = new UsernamePasswordToken(user.getUsername(), user.getPasswordHash());
+        UsernamePasswordToken token = new UsernamePasswordToken(user.getUsername(), user.getPassword());
         // 获取当前的Subject
         Subject currentUser = SecurityUtils.getSubject();
         try {
